e3d5628534e243960c65c64fbbd7c466e59e165e
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-08-20 19:27:35 +0530

    Add sum of two number

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a first number");
+        int a=sc.nextInt();
+        System.out.println("enter a second number ");
+        int b=sc.nextInt();
+        int Sum=a+b;
+        System.out.println(Sum);
+    }   
+}
