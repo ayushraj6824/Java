c49378d8c452694456c755eaf6775adc748f0cc1
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-08-24 18:49:21 +0530

    To check Conditional operator

--- Condition.java
+++ Condition.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 public class Condition {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int sum=a+b;
-        System.out.println(sum);
+        Scanner sc= new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Adult");
+        }else{
+            System.out.println("Not Adult");
+        }
 
     }
-}
+
+       
+
+    }
+
