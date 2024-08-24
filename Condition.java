4223e0143b7989ffcc2093ff362818aa245eb059
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-08-24 18:51:24 +0530

    Added to find Even or odd

--- Condition.java
+++ Condition.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Condition {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int age=sc.nextInt();
-        if(age>=18){
-            System.out.println("Adult");
+        int num=sc.nextInt();
+        if(num%2==0){
+            System.out.println("Even");
         }else{
-            System.out.println("Not Adult");
+            System.out.println("Odd");
         }
 
     }
