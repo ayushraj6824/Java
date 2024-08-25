6631f7a0d3de7f9fbe308bc64b95d792012fbc2b
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-08-25 19:50:08 +0530

    Size

---
+++ Size.java
@@ -0,0 +1,13 @@
+
+public class Size {
+    public static void main(String[] args){
+        System.out.println("char " + Character.SIZE);
+        System.out.println("byte " + Byte.SIZE);
+        System.out.println("short " + Short.SIZE);
+        System.out.println("int " + Integer.SIZE);
+        System.out.println("long " + Long.SIZE);
+        System.out.println("float " + Float.SIZE);
+        System.out.println("double " + Double.SIZE);
+    }
+    
+}
