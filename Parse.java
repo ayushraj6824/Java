bb181780fc7eb9fe9486f1103b2229e1d3dd2136
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-08-26 23:01:51 +0530

    parse

---
+++ Parse.java
@@ -0,0 +1,24 @@
+public class Parse {
+    public static void main(String[] args){
+        String str = "45";
+        String str1 = "true";
+        int a = Integer.parseInt(str);
+        double d = Double.parseDouble(str);
+        boolean b1 = Boolean.parseBoolean(str1);
+        boolean b2 = Boolean.parseBoolean(str);  
+        System.out.print(a+" "+d+" "+b1+" "+b2);
+
+
+        Integer i = 100;
+		  Double dou = 67.23;
+		  Boolean b = true;
+		  String str2 = i.toString();
+		  System.out.println(str);
+		  str = dou.toString();
+		  System.out.println(str2);
+		  str = b.toString();
+		  System.out.print(str2);
+
+        
+    }
+}
