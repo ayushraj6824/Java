6a69a43c9a0a4b8b5ba1f17d1b2198a68c4c1e13
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-09-07 23:18:13 +0530

    Added Bubble sort

--- BubbleSorting.java
+++ BubbleSorting.java
@@ -6,7 +6,7 @@ public class BubbleSorting {
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[]={7,8,3,1,2,8,9,2,6};
+        int arr[]={7,8,3,1,2,8,9,2,6,5,6,7,8,2,5};
 
 
         //bubble sort
